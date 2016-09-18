2ed4e9477700c1ed019e00a32dd60ae601eb9495
deeps09 <champ.deeps@gmail.com>
2016-09-18 08:50:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.get('/article-one', function (req, res){
 });
 
 app.get('/article-two', function (req, res){
-    res.send("Article one requested and would be served here");
+    res.send("Article two requested and would be served here");
 });
 
 app.get('/article-three', function (req, res){
-    res.send("Article one requested and would be served here");
+    res.send("Article three requested and would be served here");
 });
 
 app.get('/ui/style.css', function (req, res) {
