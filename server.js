168cffc023f4957fe4f7a34d685367bbea2d0d92
deeps09 <champ.deeps@gmail.com>
2016-09-18 10:57:18 +0530

    Add article one functioanlity in server.js instead of making html file

--- server.js
+++ server.js
@@ -7,12 +7,80 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+	title: "Article One | Deepesh Gupta",
+	heading: "Article One",
+	date: "Dated: 18th Sept 2016",
+	content:
+			`<p>
+	                This is my first article on IMAD course.
+	                I got to know about this course from newspaper and was very exited about its commencement.
+	                So far I am able to understand each and every thing clearly and will try follow along all the stuffs that I have learned
+	                from this forum.
+	            </p>
+	            
+	            <p>
+	                I have also enrolled myself for the certification exam being coonducted by IIT Madras.
+	                Such certification is very valuable for students who cannot directly qualify to study in these insititutions
+	                as they have very limited seats and ofcoure you have to be outstanding student to get admitted.
+	            </p>
+	            
+	            <p>
+	                I am very keen to learn Android devlopment as I have some prior experience in C#.net and Object Oriented Programming 
+	                Language, so I am very pleased to attend this session and want to thanks the faculty members and everyone who is directly
+	                or inderctly involved in making this course live.
+	        </p>`
+
+};
+
+function createTemplate (data){
+	var title = data.title;
+	var heading = data.heading;
+	var date = data.date;
+	var content = data.content;
+
+	var htmlTemplate = 
+					` <html>
+			    
+			    <head>
+			    <title>
+			        ${title}
+			    </title>
+			        <meta name="viewport" content="width=device-width, initialsscale=1">
+			        <link href="/ui/style.css" rel="stylesheet" />
+			    </head>
+			    
+			    <body>
+			    	<div class="container">
+				        <div>
+				            <a href = "/">Home</a>
+				        </div>
+				        <hr/>
+				        <h1>
+				            ${heading}
+				        </h1>
+				        
+				        <div>
+				            ${date}
+				        </div>
+				       
+					 <div>
+				            ${content}
+				        </div>
+			        </div>
+			    </body>
+			    
+			</html>`
+
+	return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res){
