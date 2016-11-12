66aa7b3c94077acc8f1c45de74093f678494f414
deeps09 <champ.deeps@gmail.com>
2016-11-12 07:49:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get('/test-db', function(req, res){
 app.post('/create-user', function (req, res){
     
     var username = req.body.username;
-    var password = req.body.passwoord;
+    var password = req.body.password;
 
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
