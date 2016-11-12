af99ca4bafbcad2d43b0017b0b7714cebad7df7f
deeps09 <champ.deeps@gmail.com>
2016-11-12 09:07:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.post('/create-user', function (req, res){
    });
 });
 
-app.post('/login', function (err, result){
+app.post('/login', function (req, res){
    var username = req.body.username;
    var password = req.body.password;
 
